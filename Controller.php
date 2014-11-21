e3f9b4e29bb26e7a41bb9bd5eb8f8881100ca64a
Mario Young <maye.co@gmail.com>
2014-11-21 13:05:20 -0300

    format

--- Controller.php
+++ Controller.php
@@ -15,16 +15,44 @@ use FOS\RestBundle\Controller\FOSRestController;
 use Pagerfanta\Pagerfanta;
 use Pagerfanta\Adapter\DoctrineORMAdapter;
 
+/**
+ * Class Controller
+ * @package Mayeco\BaseBundle
+ */
 abstract class Controller extends FOSRestController
 {
 
+    /**
+     * @var array
+     */
     protected $data = array();
+    
+    /**
+     * @var array
+     */
     protected $parameters = array();
 
+    /**
+     * @var string
+     */
     protected $template;
+    
+    /**
+     * @var string
+     */
     protected $format;
+    
+    /**
+     * @var
+     */
     protected $view;
 
+    /**
+     * @param Query $query
+     * @param int $page
+     * @param int $limit
+     * @return Pagerfanta
+     */
     protected function getPaginator(Query $query, $page=1, $limit=10)
     {
         $paginator = new Pagerfanta(new DoctrineORMAdapter($query));
@@ -34,17 +62,28 @@ abstract class Controller extends FOSRestController
         return $paginator;
     }
 
+    /**
+     * @param null $manager
+     * @return mixed
+     */
     protected function getDoctrineManager($manager=null)
     {
         return $this->getDoctrine()->getManager($manager);
     }
 
+    /**
+     * @param $object
+     * @param null $manager
+     * @return mixed
+     */
     protected function getRepository($object, $manager=null)
     {
-
         return $this->getDoctrineManager($manager)->getRepository(is_object($object) ? get_class($object) : $object);
     }
 
+    /**
+     * @return mixed
+     */
     protected function getDispatcher()
     {
         if (!$this->has('event_dispatcher')) {
@@ -57,7 +96,7 @@ abstract class Controller extends FOSRestController
     /**
      * Create a form from an array.
      *
-     * @param array $formArray A custom array of fields do be shown in the form
+     * @param array $formArray
      * @return Form
      */
     protected function createFormArray($formArray)
@@ -79,12 +118,20 @@ abstract class Controller extends FOSRestController
 
         return $formBuilder->getForm();
     }
-    
+
+    /**
+     * @param Form $form
+     * @param string $name
+     */
     protected function addForm(Form $form, $name='form')
     {
         $this->addData($form->createView(), $name);
     }
 
+    /**
+     * @param $data
+     * @param null $key
+     */
     protected function addData($data, $key=null)
     {
         if($key)
@@ -93,44 +140,71 @@ abstract class Controller extends FOSRestController
             $this->data[] = $data;
     }
 
+    /**
+     * @param $offset
+     */
     protected function removeData($offset)
     {
         unset($this->data[$offset]);
     }
 
+    /**
+     * @param $offset
+     * @return bool
+     */
     protected function hasData($offset)
     {
         return isset($this->data[$offset]);
     }
 
+    /**
+     * @return array
+     */
     protected function getData()
     {
         return $this->data;
     }
 
+    /**
+     * @param $template
+     */
     protected function setTemplate($template)
     {
         $this->template = $template;
     }
 
+    /**
+     * @return mixed
+     */
     protected function getTemplate()
     {
         return $this->template;
     }
 
+    /**
+     * @param $format
+     */
     protected function setFormat($format)
     {
         $this->format = $format;
     }
 
+    /**
+     * @return mixed
+     */
     protected function getFormat()
     {
         return $this->format;
     }
 
+    /**
+     * @param null $data
+     * @param null $statusCode
+     * @param array $headers
+     * @return mixed
+     */
     protected function view($data = null, $statusCode = null, array $headers = array())
     {
-
         if(null !== $data){
             $this->data[] = $data;
         }
@@ -148,6 +222,10 @@ abstract class Controller extends FOSRestController
         return $this->view;
     }
 
+    /**
+     * @param $object
+     * @return mixed
+     */
     protected function validate($object)
     {
         if (!$this->has('validator')) {
@@ -157,14 +235,22 @@ abstract class Controller extends FOSRestController
         return $this->get('validator')->validate($object);
     }
 
+    /**
+     * @param $object
+     * @param bool $exit
+     */
     protected function debug($object, $exit=true)
     {
-
         Debug::dump($object);
         if($exit)
             exit();
     }
 
+    /**
+     * @param $object
+     * @param bool $flush
+     * @param null $manager
+     */
     protected function persist($object, $flush=false, $manager=null)
     {
         if(null === $object){
@@ -177,32 +263,50 @@ abstract class Controller extends FOSRestController
         }
     }
 
+    /**
+     * @param null $manager
+     */
     protected function flush($manager=null)
     {
-
         $this->getDoctrineManager($manager)->flush();
     }
 
+    /**
+     * @param $object
+     * @param null $manager
+     */
     protected function update($object, $manager=null)
     {
         $this->persist($object, true, $manager);
     }
 
+    /**
+     * @param $object
+     * @param null $manager
+     * @return mixed
+     */
     protected function merge($object, $manager=null)
     {
-        
         return $this->getDoctrineManager($manager)->merge($object);
     }
 
+    /**
+     * @param $object
+     * @param null $manager
+     * @return mixed
+     */
     protected function detach($object, $manager=null)
     {
-        
         return $this->getDoctrineManager($manager)->detach($object);
     }
 
+    /**
+     * @param $object
+     * @param bool $flush
+     * @param null $manager
+     */
     protected function remove($object, $flush=false,$manager=null)
     {
-
         if(null === $object){
             $this->error('no null update');
         }
@@ -213,19 +317,28 @@ abstract class Controller extends FOSRestController
         }
     }
 
+    /**
+     * @param $eventName
+     * @param Event $event
+     * @return mixed
+     */
     protected function dispatch($eventName, Event $event = null)
     {
-
         return $this->getDispatcher()->dispatch($eventName, $event);
-
     }
 
-    protected function NotFound($message="Error 404"){
-
+    /**
+     * @param string $message
+     */
+    protected function NotFound($message="Error 404")
+    {
         throw $this->createNotFoundException($message);
-
     }
 
+    /**
+     * @param $condition
+     * @param string $message
+     */
     protected function NotFoundUnless($condition, $message = 'Error 404')
     {
         if (!$condition)
@@ -234,6 +347,10 @@ abstract class Controller extends FOSRestController
         }
     }
 
+    /**
+     * @param $condition
+     * @param string $message
+     */
     protected function NotFoundIf($condition, $message = 'Error 404')
     {
         if ($condition)
@@ -242,40 +359,51 @@ abstract class Controller extends FOSRestController
         }
     }
 
+    /**
+     * @param string $message
+     */
     protected function AccessDenied($message='Unable to access this page!')
     {
-
         throw $this->createAccessDeniedException($message);
-
     }
 
+    /**
+     * @param $condition
+     * @param string $message
+     */
     protected function AccessDeniedIf($condition, $message='Unable to access this page!')
     {
-
         if ($condition)
         {
             $this->AccessDenied($message);
         }
-
     }
 
+    /**
+     * @param $condition
+     * @param string $message
+     */
     protected function AccessDeniedUnless($condition, $message='Unable to access this page!')
     {
-
         if (!$condition)
         {
             $this->AccessDenied($message);
         }
-
     }
 
+    /**
+     * @param string $message
+     */
     protected function error($message='error in your application!')
     {
-
         throw new \LogicException($message);
-
     }
 
+    /**
+     * @param Form $form
+     * @param Request $request
+     * @return bool
+     */
     protected function handleForm(Form $form, Request $request)
     {
         $form->handleRequest($request);
@@ -284,8 +412,11 @@ abstract class Controller extends FOSRestController
         }
     }
 
-    protected function setParameters(){
-
+    /**
+     *
+     */
+    protected function setParameters()
+    {
         $attributes = $this->getRequest()->attributes->all();
         if($attributes['paramFetcher']) {
             $this->parameters = array_merge($attributes, $attributes['paramFetcher']->all());
@@ -294,23 +425,31 @@ abstract class Controller extends FOSRestController
 
     }
 
+    /**
+     * @param $name
+     * @param null $dir
+     * @param bool $first
+     * @return null|void
+     */
     protected function getFilename($name, $dir = null, $first = true)
     {
-        
         $path = null;
-        
+
         try {
-            
+
             $path = $this->kernel()->locateResource($name, $dir, $first);
-            
+
         } catch (\Exception $e) {
-            
+
             return;
         }
 
         return $path;
     }
 
+    /**
+     * @return mixed
+     */
     protected function kernel()
     {
         if (!$this->has('kernel')) {
@@ -319,12 +458,18 @@ abstract class Controller extends FOSRestController
 
         return $this->get('kernel');
     }
-    
+
+    /**
+     * @return mixed
+     */
     protected function getRootDir()
     {
         return $this->kernel()->getRootDir();
     }
 
+    /**
+     * @return mixed
+     */
     protected function session()
     {
         if (!$this->has('session')) {
@@ -334,16 +479,28 @@ abstract class Controller extends FOSRestController
         return $this->get('session');
     }
 
+    /**
+     * @param $key
+     * @return mixed
+     */
     protected function getsession($key)
     {
         return $this->session()->get($key);
     }
 
+    /**
+     * @param $key
+     * @param $data
+     * @return mixed
+     */
     protected function setsession($key, $data)
     {
         return $this->session()->set($key, $data);
     }
 
+    /**
+     * @return mixed
+     */
     protected function mailer()
     {
         if (!$this->has('mailer')) {
@@ -353,6 +510,9 @@ abstract class Controller extends FOSRestController
         return $this->get('mailer');
     }
 
+    /**
+     * @return mixed
+     */
     protected function security()
     {
         if (!$this->has('security.context')) {
@@ -362,6 +522,9 @@ abstract class Controller extends FOSRestController
         return $this->get('security.context');
     }
 
+    /**
+     * @return mixed
+     */
     protected function memcache()
     {
         if (!$this->has('memcache.default')) {
@@ -371,20 +534,34 @@ abstract class Controller extends FOSRestController
         return $this->get('memcache.default');
     }
 
+    /**
+     * @param $key
+     * @return mixed
+     */
     protected function getmemcache($key)
     {
         return $this->memcache()->get($key);
     }
 
+    /**
+     * @param $key
+     * @param $data
+     * @param int $time
+     * @return mixed
+     */
     protected function setmemcache($key, $data, $time = 86400)
     {
         return $this->memcache()->set($key, $datacache, $time);
     }
-    
+
+    /**
+     * @param $message
+     * @param string $type
+     * @return mixed
+     */
     protected function addflash($message, $type="notice")
     {
-        
         return $this->session()->getFlashBag()->add($type, $message);
     }
-    
+
 }
